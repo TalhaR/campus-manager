ccc56714923c6891b435ca2b8174ebdbf3ff6822
TalhaR <delta.talha@gmail.com>
2021-05-06 22:17:59 -0400

    updated models

--- backend/database/utils/seedDB.js
+++ backend/database/utils/seedDB.js
@@ -3,11 +3,13 @@ const { Campus, Student } = require('../models');
 const seedDB = async () => {
 	const dummy_campus = await Campus.create({
 		name: "Hunter College",
-		description: "This is a school in NYC."
+		description: "This is a school in NYC.",
+		address: "1",
 	});
 	const dummy_campus2 = await Campus.create({
 		name: "Harvard",
-		description: "This is a school in MA."
+		description: "This is a school in MA.",
+		address: "12",
 	});
 
 	const dummy_student = await Student.create({
