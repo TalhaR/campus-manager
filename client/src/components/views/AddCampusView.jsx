b06bbc1b6c6540db2aa97fbd8b8bd71d6f5f4520
xshi0603 <xshi603@gmail.com>
2021-05-13 20:48:14 -0400

    added a form for creating a campus that doesnt fully work atm

--- client/src/components/views/AddCampusView.jsx
+++ client/src/components/views/AddCampusView.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Navbar } from '../containers/Navbar'
 
 const AddCampusView = () => {
   return (
     <div>
+      <Navbar />
       add campus
     </div>
   )
--- client/src/components/views/AddStudentView.jsx
+++ client/src/components/views/AddStudentView.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { React, useState } from 'react'
+import { Navbar } from '../containers/Navbar'
+import Grid from '@material-ui/core/Grid';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+}));
 
 const AddStudentView = () => {
+  const classes = useStyles();
+  const [campusName, setCampusName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('campusName:', campusName);
+  }
+
   return (
     <div>
-      add student
+      <Navbar />
+      <Grid container justify="center">
+        <h1>New Campus Form</h1>
+      </Grid>
+  
+      <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <Grid container justify="center">
+          <TextField id="standard-basic" label="Campus Name" value={campusName} onInput={ e=>setCampusName(e.target.value)}/>
+        </Grid>
+
+        <Grid container justify="center">
+          <Button type="submit" variant="contained" color="primary">
+            Submit
+          </Button>
+        </Grid>
+      </form>
     </div>
   )
 }
