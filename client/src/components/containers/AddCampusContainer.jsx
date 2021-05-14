3d540e78326ee9672b9f374b8389ec150e91c8f6
xshi0603 <xshi603@gmail.com>
2021-05-13 22:55:22 -0400

    done with adding students

--- client/src/components/containers/AddCampusContainer.jsx
+++ client/src/components/containers/AddCampusContainer.jsx
@@ -1,12 +1,66 @@
-import React from 'react'
+import { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { fetchAllCampusesThunk, addCampusThunk } from "../../store/thunks";
 import { AddCampusView } from "../views";
 
-const AddCampus = () => {
-  return (
-    <div>
-      <AddCampusView />
-    </div>
-  )
+class AddCampusesContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      campusName: '',
+    };
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const campus = {name: this.state.campusName, address: "No Current Address"};
+    let url = window.location.href;
+    url = url.substring(0, url.lastIndexOf('/'));
+    let newCampus =  await this.props.addCampusThunk(campus);
+    window.location.href = url + '/campus/' + newCampus.id;
+  }
+
+  setCampusName = (newCampusName) => {
+    this.setState({campusName: newCampusName});
+  }
+
+  componentDidMount() {
+    console.log(this.props);
+  }
+
+  render() {
+    return (
+      <AddCampusView
+        campusName={this.state.campusName}
+        handleSubmit={this.handleSubmit}
+        setCampusName={this.setCampusName}
+        handleTest={this.handleTest}
+      />
+    );
+  }
 }
 
-export default AddCampus
+// Map state to props;
+const mapState = (state) => {
+  return {
+    allCampuses: state.allCampuses,
+  };
+};
+
+// Map dispatch to props;
+const mapDispatch = (dispatch) => {
+  return {
+    fetchAllCampuses: () => dispatch(fetchAllCampusesThunk()),
+    addCampusThunk: (campus) => dispatch(addCampusThunk(campus)),
+  };
+};
+
+// Type check props;
+AddCampusesContainer.propTypes = {
+  allCampuses: PropTypes.array.isRequired,
+  fetchAllCampuses: PropTypes.func.isRequired,
+};
+
+// Export our store-connected container by default;
+export default connect(mapState, mapDispatch)(AddCampusesContainer);
--- client/src/components/views/AddStudentView.jsx
+++ client/src/components/views/AddStudentView.jsx
@@ -1,7 +1,7 @@
 import { React, useState } from 'react'
+import axios from 'axios';
 import { Navbar } from '../containers/Navbar'
 import Grid from '@material-ui/core/Grid';
-import FormControl from '@material-ui/core/FormControl';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
@@ -24,6 +24,23 @@ const AddStudentView = () => {
     console.log('campusName:', campusName);
   }
 
+  const handleTest = async (e) => {
+    console.log("testing");
+    const res = await axios.get("/api/campuses/");
+    console.log(res.data);
+    console.log("done testings");
+  }
+  /*
+  await axios.post("/api/favorites", null, {
+    params: {
+      orgName: name || "",
+      orgId: ein,
+      userId: user.uid,
+      orgAddress: organization.orgAddress || "",
+      tagLine: organization.tagLine || "",
+    },
+  });
+  */
   return (
     <div>
       <Navbar />
@@ -39,6 +56,12 @@ const AddStudentView = () => {
         <Grid container justify="center">
           <Button type="submit" variant="contained" color="primary">
             Submit
+          </Button>
+        </Grid>
+
+        <Grid container justify="center">
+          <Button variant="contained" color="primary" onClick={handleTest}>
+            Test Button
           </Button>
         </Grid>
       </form>
