4f1e51776c24d99674d9e2e8dd4f4fd7eab096f8
Darren Liang <liangdarren2@gmail.com>
2021-05-18 09:48:24 -0400

    campus listing grid view

--- client/src/components/views/AllCampusesView.js
+++ client/src/components/views/AllCampusesView.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { Navbar } from '../containers/Navbar'
-
+import { Container, Typography, Card, Button, Grid} from '@material-ui/core/'
 
 const AllCampusesView = (props) => {
   if (!props.allCampuses.length) {
@@ -9,20 +9,34 @@ const AllCampusesView = (props) => {
   }
 
   return (
-    <div>
+    <>
       <Navbar />
+      <Container>
+      <Typography variant='h1' align='center'>Campus Listing</Typography>
       <Link to={`/addcampus`}>
         <h1>Add Campus</h1>
       </Link>
+      <Grid container spacing={3}>
+      
+      
       {props.allCampuses.map((campus) => (
-        <div key={campus.id}>
-          <Link to={`/campus/${campus.id}`}>
-            <h1>{campus.name}</h1>
-          </Link>
-          <p>{campus.description}</p>
-        </div>
+        <Grid item xs={6}>
+        <Card variant="outlined" key={campus.id}>
+          <img src={campus.imageURL} alt="campus image"></img>
+          <Typography variant='h4' align='center'>
+            <Link to={`/campus/${campus.id}`}>{campus.name}</Link>
+          </Typography>
+          <Typography variant='subtitle1' align='center'>{campus.description}</Typography>
+          <Typography align='right'>
+            <Button>edit</Button>
+            <Button>delete</Button>
+          </Typography>
+        </Card>
+        </Grid>
       ))}
-    </div>
+      </Grid>
+      </Container>
+    </>
   );
 };
 
