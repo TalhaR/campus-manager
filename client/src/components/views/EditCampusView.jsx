57cd7690bf75268453711b447452fe549c1a3c7f
Aamer Shikari <shikariaamer@gmail.com>
2021-05-23 23:15:16 -0400

    Addition of edit campus view students and addition of students to a campus

--- client/src/components/views/EditCampusView.jsx
+++ client/src/components/views/EditCampusView.jsx
@@ -3,7 +3,10 @@ import { Navbar } from '../containers/Navbar'
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from "react-router-dom";
-import { Typography, Card, Button, Grid } from '@material-ui/core/'
+import { Select, Container, Typography, Card, Button, Grid, MenuItem } from '@material-ui/core/'
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,6 +19,30 @@ const useStyles = makeStyles((theme) => ({
 
 const EditCampusView = (props) => {
   const classes = useStyles();
+  const [studentId, setStudentId] = useState(null);
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+    setStudentId(e.target.value);
+  };
+
+  useEffect(() => { 
+    console.log(props.campus);
+  }, [props.campus])
+
+
+  const handleAddSubmit = (e) => {
+    console.log(e.target.value)
+    e.preventDefault();
+    if (studentId)
+      axios
+        .put(`/api/students/${studentId}`, { campusId: props.campus.id })
+        .then((e) => console.log(e));
+        //e.target.submit());
+        
+        console.log(studentId);
+        
+  };
 
   return (
     <div>
@@ -62,42 +89,67 @@ const EditCampusView = (props) => {
         </Grid>
 
         <Grid container justify="center">
+            
           <Button type="submit" variant="contained" color="primary">
             Save Changes
           </Button>
+          </Grid>
+        </form>
+        <Grid container justify='center' >
+            <Grid item xs={6}>
+                <form onSubmit={handleAddSubmit}>
+                <Select
+
+                    defaultValue=""
+                    onChange={handleChange}
+                >
+                    <MenuItem value="" disabled hidden>
+                    <em>None</em>
+                    </MenuItem>
+                    {props.students && props.students.length && 
+                    props.students.map((student) => (
+                        <MenuItem key={student.id} value={student.id}>
+                        <em>{student.firstname + " " + student.lastname}</em>
+                        </MenuItem>
+                    ))}
+                </Select>
+                <Button type="submit" value="Select Campus...">Change Campus</Button>
+                </form>
+            </Grid>
         </Grid>
 
+        <Container>
+            <Typography variant='h4' align='left'>Students on Campus</Typography>
 
-
-        {/* <Typography variant='h4' align='left'>Students on Campus</Typography>
-
-        <Grid container spacing={3}>
-            <Grid item xs={6}>
-                {props.campus.students.map(student => {
-                let name = student.firstname + " " + student.lastname;
-                return (
-                    <Card key={student.id}>
-                    <img align='center' src={student.imageUrl} alt={name} style={{width: "100%"}} />
-                    <Typography variant='subtitle1' align='center'>
-                        <Link to={`/student/${student.id}`}>
-                        {name}
-                        </Link>
-                    </Typography>
-                    <Typography variant='subtitle2' align='center'>
-                        {props.campus.name}
-                    </Typography>
-                    </Card>
-                );
-                })}
-            </Grid>
-        </Grid> */}
-      </form>
+            <Grid container spacing={3}>
+                <Grid item xs={6}>
+                    {props.campus.students.map(student => {
+                    let name = student.firstname + " " + student.lastname;
+                    return (
+                        <Card key={student.id}>
+                        <img align='center' src={student.imageUrl} alt={name} style={{width: "100%"}} />
+                        <Typography variant='subtitle1' align='center'>
+                            <Link to={`/student/${student.id}`}>
+                            {name}
+                            </Link>
+                        </Typography>
+                        <Typography variant='subtitle2' align='center'>
+                            {props.campus.name}
+                        </Typography>
+                        </Card>
+                    );
+                    })}
+                </Grid>
+            </Grid> 
+        </Container>
+      
     </div>
   )
 }
 
 EditCampusView.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
+  handleAddSubmit: PropTypes.func.isRequired,
   name: PropTypes.string.isRequired,
   address: PropTypes.string.isRequired,
   imageUrl: PropTypes.string.isRequired,
